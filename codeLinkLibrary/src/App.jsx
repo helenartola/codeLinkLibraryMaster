03ff59098f16995eebe782fc01a51d47301eb6f6
Helena Artola <116822570+helenartola@users.noreply.github.com>
2024-01-21 16:08:31 +0100

    Hemos añadido estilo, componentes, rutas...

--- codeLinkLibrary/src/App.jsx
+++ codeLinkLibrary/src/App.jsx
@@ -1,11 +1,31 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import HomePage from "./pages/homepage/HomePage";
+import RegisterPage from "./pages/registerpage/RegisterPage";
+import LoginPage from "./pages/loginpage/LoginPage";
+import PostPage from "./pages/postpage/PostPage";
+import NotFoundPage from "./pages/notfoundpage/NotFoundPage";
 
 function App() {
   return (
     <>
-      <div>
-        <img className="logo" src="/LogotipoCLL-blanco.png"></img>
-      </div>
+      <main className="main-container">
+        <Header />
+        <div className="description">
+          <h2>Elevate your web development journey </h2>
+          <h3>Share, learn, and collaborate seamlessly</h3>
+        </div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/registro" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/post/:id" element={<PostPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+        <Footer />
+      </main>
     </>
   );
 }
